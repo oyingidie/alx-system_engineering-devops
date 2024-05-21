43bd8d270b9d2e8022d4fbcdd369b8f01dc3403f
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-21 09:10:50 -0700

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that exports data in CSV format"""
+
+if __name__ == "__main__":
+    import requests
+    import csv
+    from sys import argv
+
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1]))
+    username = r.json().get('username')
+
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                     format(argv[1]))
+    data = r.json()
+
+    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in data:
+            csv_writer.writerow([argv[1], username, task.get('completed'),
+                                 task.get('title')])
