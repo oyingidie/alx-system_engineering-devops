6cb956b643e3ce858738395313b5bc38ea9db28c
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-21 09:03:34 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                 completed += 1
 
     print('Employee {} is done with tasks({}/{}):'
-          .format(employee_name, completed, allTasks))
+          .format(employeeName, completed, allTasks))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
           if task.get('userId') == int(userId) and task.get('completed')]))
